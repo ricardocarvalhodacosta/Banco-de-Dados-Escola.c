8d15caec939187908bc037ac568c6252ef7effa7
RIcardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-03-13 20:26:30 -0300

    Inicio do projeto
    Hoje comecei estudando as funções que vou utilizar para o programa e já fui testando alguns métodos no caminho, por enquanto deixarei só a estrutura básica do programa;

---
+++ banco-de-notas.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Programa para calcular e armazenar as notas de alunos de diferentes salas
+//A ideia é ter ao menos 3 arquivos .txt com as notas e médias dos alunos e suas respectivas mátriculas.
+
+int main () {
+
+    FILE *classes;
+
+    return (0);
+}
