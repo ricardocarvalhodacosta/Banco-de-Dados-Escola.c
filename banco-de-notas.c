e78099c30382b75a13d280b22a8359fe22cf98a4
RIcardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-03-16 21:21:58 -0300

    Recomeçando
    Hoje eu decidi recomeçar, estava meio perdido e não estava conseguindo achar uma solução, então irei recomeçar e fazer parte por parte para construir um programa inteiro!

--- banco-de-notas.c
+++ banco-de-notas.c
@@ -1,130 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-struct notas
-{
-    char nome[40];
-    int matricula;
-    char nota[40];
-};
 
 typedef struct notas notas;
 
 int main (void) {
-
-    notas dados;
-    int menu;
-    int i, A, B, C;
-    FILE *salas;
     
-    printf("Olá, qual classe você gostaria de acessar?\n");
-    printf("1. Classe A, 2. Classe B ou 3. Classe C?\n\n");
-    scanf("Digite a Letra da Classe = %d\n\n", &i);
+    FILE *arquivo;
+    char nome[40];
+    double nota;
 
-
-    if (i==A) {
-        printf("Olá, o que você gostaria de fazer?\n");
-        printf("1. Adicionar um novo aluno?;\n");
-        printf("2. Verificar os alunos cadastrados?;\n");
-        scanf("%d", &menu);
-
-        if (menu == 1)
-        {
-             salas = fopen ("classeA.txt", "a");
-            printf("Digite o nome do Aluno: \n");
-            fgets(dados.nome, 40, stdin);
-            fprintf(salas, "Aluno: %s\n", dados.nome);
-            printf("Agora digite a matricula do Aluno: \n");
-            scanf("%d", &dados.matricula);
-            fprintf(salas, "Número da Matricula: %d\n", dados.matricula);
-            printf("Qual a matéria e nota gostaria de adicionar? \n");
-            fgets(dados.nota, 40, stdin);
-            fprintf(salas, "Materia e Nota: %s", dados.nota);
-            printf("Os dados são!\n");
-            printf("Nome: %s, Número Matricula: %d e Materia/Nota: %s.\n", dados.nome, dados.matricula, dados.nota);
-            printf("Dados salvos com sucesso! Obrigado.\n\n");
-            fclose(salas);
-        } else {
-            char dados_str[100];
-            salas = fopen ("classeA.txt", "r");
-            printf("Carregando os dados...\n\n");
-            printf("Os dados são: \n\n");
-            while (fgets(dados_str, 100, salas) != NULL)
-            {
-                printf("%s", dados_str);
-            }
-            fclose(salas);
-        }    
-    } else {
-        printf("Olá, o que você gostaria de fazer?\n");
-        printf("1. Adicionar um novo aluno?;\n");
-        printf("2. Verificar os alunos cadastrados?;\n");
-        scanf("%d", &menu);
-
-        if (menu == 1)
-        {
-            salas = fopen ("classeB.txt", "w");
-            printf("Digite o nome do Aluno: \n");
-            fgets(dados.nome, 40, stdin);
-            fprintf(salas, "Aluno: %s\n", dados.nome);
-            printf("Agora digite a matricula do Aluno: \n");
-            scanf("%d", &dados.matricula);
-            fprintf(salas, "Número da Matricula: %d\n", dados.matricula);
-            printf("Qual a matéria e nota gostaria de adicionar? \n");
-            fgets(dados.nota, 40, stdin);
-            fprintf(salas, "Materia e Nota: %s", dados.nota);
-            printf("Os dados são!\n");
-            printf("Nome: %s, Número Matricula: %d e Materia/Nota: %s.\n", dados.nome, dados.matricula, dados.nota);
-            printf("Dados salvos com sucesso! Obrigado.\n\n");
-            fclose(salas);
-        } else {
-            char dados_str[100];
-            salas = fopen ("classeB.txt", "r");
-            printf("Carregando os dados...\n\n");
-            printf("Os dados são: \n\n");
-            while (fgets(dados_str, 100, salas) != NULL)
-            {
-                printf("%s", dados_str);
-            }
-            fclose(salas);
-        }    
-    }
-    
-    if (i == C)
-    {
-        printf("Olá, o que você gostaria de fazer?\n");
-        printf("1. Adicionar um novo aluno?;\n");
-        printf("2. Verificar os alunos cadastrados?;\n");
-        scanf("%d", &menu);
-
-        if (menu == 1)
-        {
-            salas = fopen ("classeC.txt", "w");
-            printf("Digite o nome do Aluno: \n");
-            fgets(dados.nome, 40, stdin);
-            fprintf(salas, "Aluno: %s\n", dados.nome);
-            printf("Agora digite a matricula do Aluno: \n");
-            scanf("%d", &dados.matricula);
-            fprintf(salas, "Número da Matricula: %d\n", dados.matricula);
-            printf("Qual a matéria e nota gostaria de adicionar? \n");
-            fgets(dados.nota, 40, stdin);
-            fprintf(salas, "Materia e Nota: %s", dados.nota);
-            printf("Os dados são!\n");
-            printf("Nome: %s, Número Matricula: %d e Materia/Nota: %s.\n", dados.nome, dados.matricula, dados.nota);
-            printf("Dados salvos com sucesso! Obrigado.\n\n");
-            fclose(salas);
-        } else {
-            char dados_str[100];
-            salas = fopen ("classeC.txt", "r");
-            printf("Carregando os dados...\n\n");
-            printf("Os dados são: \n\n");
-            while (fgets(dados_str, 100, salas) != NULL)
-            {
-                printf("%s", dados_str);
-            }
-            fclose(salas);
-        }    
-    }
+    arquivo = fopen ("Classe.txt", "w");
+    printf("Arquivo aberto com sucesso!\n");
+    printf("Adicione o nome e a nota do aluno:\n");
+    fgets(nome, 40, stdin);
+    printf("Agora a nota do aluno:\n");
+    scanf("%d", &nota);
+    printf("Armazenando os dados computados...\n");
+    printf("...\n");
+    printf("..\n");
+    printf(".\n");
+    printf("Dados armazenados com sucesso!");
+    fprintf(arquivo, "Nome: %s\n", nome);
+    fprintf(arquivo, "Nota: %d\n\n", nota);
+    fclose(arquivo);
 
     return (0);
 }
